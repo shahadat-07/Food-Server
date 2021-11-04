c99aec04d2ef11f783c9f48e3b734d84939c92fb
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2021-11-04 19:00:16 +0600

    Home, Login and Registration UI are nearly Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Admin from "./components/Admin/Admin/Admin";
+import NewMenu from "./components/Admin/NewMenu/NewMenu";
+import NewRestaurant from "./components/Admin/NewRestaurant/NewRestaurant";
+import Home from "./components/Home/Home/Home";
+import Login from "./components/Login/Login";
+import Registration from "./components/Registration/Registration";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Registration />
+        </Route>
+        <Route exact path="/newRestaurant">
+          <NewRestaurant />
+        </Route>
+        <Route exact path="/newMenu">
+          <NewMenu />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
